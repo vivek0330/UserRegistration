c2666e06c2f4de1d9be558e2efe48577ea11c404
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-28 04:48:50 +0530

    Show: initial comment

---
+++ UserRegistration/src/com/user/UserRegistration.java
@@ -0,0 +1,19 @@
+/**
+* <h1>Welcome to the user registration problem !!</h1> 
+* The user registration problem implements an application that
+* simply displays "Welcome to the user registration problem !!" to the standard output.
+* 
+* @author Official_vk
+* @version 1.0
+* @since 2021-07-28
+*/
+package com.user;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		// Prints Welcome to the user registration problem !! on standard output.
+		System.out.println("Welcome to the user registration problem !!");
+	}
+
+}
